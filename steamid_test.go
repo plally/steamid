472f3eb2e23a7c1f8af2d40e1e93bb5e8fe58193
Pierce <pierce@vulpes.dev>
2024-01-10 00:33:49 -0500

    improve tests

--- steamid_test.go
+++ steamid_test.go
@@ -4,21 +4,46 @@ import (
 	"testing"
 )
 
-func TestSteamID64(t *testing.T) {
+func TestSteamID32(t *testing.T) {
 	data := []struct {
-		steamid  string
-		expected int64
+		steamid32 string
+		steamid64 string
+		steamid3  string
+		expected  int64
 	}{
-		{"STEAM_0:0:450041545", 76561198860348818},
+		{"STEAM_0:0:450041545", "76561198860348818", "[U:1:900083090]", 76561198860348818},
+		{"STEAM_0:0:11101", "76561197960287930", "[U:1:22202]", 76561197960287930},
 	}
 
 	for _, v := range data {
-		out, err := SteamID32(v.steamid)
-		if err != nil {
-			t.Fatal(err)
-		}
-		if int64(out) != v.expected {
-			t.Fatalf("Expected %d, got %d", v.expected, out)
-		}
+		t.Run("SteamID32", func(t *testing.T) {
+			out, err := SteamID32(v.steamid64)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if int64(out) != v.expected {
+				t.Fatalf("Expected %d, got %d", v.expected, out)
+			}
+		})
+
+		t.Run("SteamID64", func(t *testing.T) {
+			out, err := SteamID64(v.steamid64)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if int64(out) != v.expected {
+				t.Fatalf("Expected %d, got %d", v.expected, out)
+			}
+		})
+
+		t.Run("SteamID3", func(t *testing.T) {
+			out, err := SteamID3(v.steamid3)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if int64(out) != v.expected {
+				t.Fatalf("Expected %d, got %d", v.expected, out)
+			}
+		})
 	}
 }
